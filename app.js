9968f15962075f9eda2a35719dc75d65baa4d376
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-27 12:54:01 -0500

    FizzbuzzService service refactoring

--- app.js
+++ app.js
@@ -1,63 +1,32 @@
 const Reader = require('./lib/utils/Reader')
 const ExplorerService = require('./lib/services/ExplorerService')
+const FizzbuzzService = require('./lib/services/FizzbuzzService')
 
 const explorers = Reader.readJsonFile("explorers.json")
 // console.log(explorers)
 
 
 const explorersNode = ExplorerService.filterByMission(explorers, "node");
-// console.log(explorersNode)
+console.log(explorersNode)
+
 const explorersAmount = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-// console.log(explorersAmount)
+console.log(explorersAmount)
+
 const userNameExplorersByMission = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
-// console.log(userNameExplorersByMission)
+console.log(userNameExplorersByMission)
 
+const explorer1 = {name: "Explorer1", score: 1}
+const result1 = FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+console.log(result1)
 
-// Part 5: Get a new list of explorers in node, if the score numbers is divisible by 3, I need a new propery called trick, and the value assigned is FIZZ, if not the value should be the score itself.
-// Score: 3, Trick: FIZZ.
-// Score: 4, Trick: 4.
-// Score: 5, Trick: 5.
+const explorer3 = {name: "Explorer3", score: 3}
+const result2 = FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+console.log(result2)
 
-const assignFizzTrick = function(explorer){
-    if(explorer.score%3 === 0){
-        explorer.trick = "FIZZ";
-        return explorer;
-    }else{
-        explorer.trick = explorer.score;
-        return explorer;
-    }
-};
+const explorer5 = {name: "Explorer5", score: 5}
+const result3 = FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+console.log(result3)
 
-// const explorersInNodeAndFizzTrick = explorersInNode.map((explorer) => assignFizzTrick(explorer));
-// console.log(explorersInNodeAndFizzTrick)
-
-// Part 6: Get a new list of explorers in node if the score number is divisible by 5, we need to set a new property called trick and set the value BUZZ, if not this value should be just the score
-//
-const assignBuzzTrick = function(explorer){
-    if(explorer.score%5 === 0){
-        explorer.trick = "BUZZ";
-        return explorer;
-    }else{
-        explorer.trick = explorer.score;
-        return explorer;
-    }
-};
-
-// const explorersInNodeAndBuzzTrick = explorersInNode.map((explorer) => assignBuzzTrick(explorer));
-// console.log(explorersInNodeAndBuzzTrick)
-
-//Part7: Get a new list of explorers in Node, if the score number is divisible by 3 AND by 5 we need to set a new property called FIZZBUZZ, if not this value should be the same score value
-
-const assignFizzBuzzTrick = function(explorer){
-    if(explorer.score%5 === 0 && explorer.score%3 === 0){
-        explorer.trick = "FIZZBUZZ";
-        return explorer;
-    }else{
-        explorer.trick = explorer.score;
-        return explorer;
-    }
-};
-
-// const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) => assignFizzBuzzTrick(explorer));
-
-// Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODOA
+const explorer15 = {name: "Explorer15", score: 15}
+const result4 = FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+console.log(result4)
