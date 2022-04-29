f73989e44cc905d2943c36eeb93a61e19232e6c6
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-28 21:25:52 -0500

    Step 13: Add test for ExplorerController

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,18 @@
+const ExplorerController = require('../../lib/controllers/ExplorerController')
+
+describe("Test for ExplorerController", () => {
+    test("Request 1: Search explorers by mission by parameters", () => {
+        const explorerInJava = ExplorerController.getExplorerByMission('java')
+        expect(explorerInJava.length).not.toBe(0)
+    });
+
+    test("Request 2: Get username of eplorers by mission", () => {
+        const usernames = ExplorerController.getExplorersUsernamesByMission('node')
+        expect(usernames.length).not.toBe(0)
+    })
+
+    test("Request 3: Get amount of eplorers by mission", () => {
+        const usernames = ExplorerController.getAmountOfExplorersByMission('node')
+        expect(usernames).not.toBe(0)
+    })
+});
--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -4,22 +4,22 @@ const FizzbuzzService = require("../../lib/services/FizzbuzzService");
 describe("Test fonr FizzbuzzService", () => {
     test("Request 1: Apply Validation In Explorer 1", () => {
         const explorer1 = { name: "Explorer1", score: 1 };
-        const result1 = FizzbuzzService.applyValidationInExplorer(explorer1); // {name: "Explorer1", score: 1, trick: 1} 
+        const result1 = FizzbuzzService.applyValidationInExplorer(explorer1); 
         expect(result1).toEqual({ name: "Explorer1", score: 1, trick: 1 });
     });
     test("Request 1: Apply Validation In Explorer 3", () => {
         const explorer3 = { name: "Explorer3", score: 3 };
-        const result2 = FizzbuzzService.applyValidationInExplorer(explorer3); // {name: "Explorer3", score: 3, trick: "FIZZ"}
+        const result2 = FizzbuzzService.applyValidationInExplorer(explorer3);
         expect(result2).toEqual({name: "Explorer3", score: 3, trick: "FIZZ"});
     });
     test("Request 1: Apply Validation In Explorer 5", () => {
         const explorer5 = { name: "Explorer5", score: 5 };
-        const result3 = FizzbuzzService.applyValidationInExplorer(explorer5); // {name: "Explorer5", score: 5, trick: "BUZZ"}
+        const result3 = FizzbuzzService.applyValidationInExplorer(explorer5);
         expect(result3).toEqual({name: "Explorer5", score: 5, trick: "BUZZ"});
     });
     test("Request 1: Apply Validation In Explorer 15", () => {
         const explorer15 = { name: "Explorer15", score: 15 };
-        const result4 = FizzbuzzService.applyValidationInExplorer(explorer15); // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+        const result4 = FizzbuzzService.applyValidationInExplorer(explorer15);
         expect(result4).toEqual({name: "Explorer15", score: 15, trick: "FIZZBUZZ"});
 
     });
